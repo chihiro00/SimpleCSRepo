e3435c1c82e4442d413296c144ca28bef0c58edc
Joanna Augustyn <joanna.augustyn1997@gmail.com>
2021-08-08 23:16:31 +0200

    initial commit

---
+++ ConsoleAppAsia07Figury/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleAppAsia07Figury
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
